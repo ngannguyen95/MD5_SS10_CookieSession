2bf92c7ddcfc6db9f20e4cbdd1b6c1b502a658ee
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-05-31 07:59:53 +0700

    hihi

---
+++ theanPhamVaoGioHang/src/main/java/ra/model/Cart.java
@@ -0,0 +1,12 @@
+package ra.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+    private Map<Product,Integer> product = new HashMap<>();
+
+    public Cart() {
+    }
+
+}
